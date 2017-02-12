a328cf180ee50a2a9cb0c8ed1ac49210a0849e3f
merritt duncan <merrittduncan@gmail.com>
2017-02-12 12:39:40 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import webapp2
+import cgi
+import jinja2
+import os
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+    def write (self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template,**kw))
+
+class BlogPosts(db.Model):
+    title = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+class Index(Handler):
+
+    def render_front(self, title="", content="", error=""):
+        posts = db.GqlQuery("SELECT * from BlogPosts ORDER BY created DESC")
+
+        self.render("front.html", title=title, content=content, error=error, posts=posts)
+
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        content = self.request.get("content")
+
+        if title and content:
+            a = BlogPosts(title=title, content=content)
+            a.put()
+
+            self.redirect("/")
+
+        else:
+            error = "try again, fool"
+            self.render_front(title,content,error)
+
+class Blog(Handler):
+
+    def get(self):
+        recentPosts = db.GqlQuery("SELECT * from BlogPosts ORDER BY created DESC LIMIT 5")
+
+        self.render("recent.html", posts=recentPosts)
+
+app = webapp2.WSGIApplication([
+    ('/', Index),
+    ('/blog', Blog)
+], debug=True)
